ce9336d91306e84d0d05e6cc44dcc3da374f23e7
Kyung Jin Cho <kj_cho@idealidea.co.kr>
2019-05-18 13:48:07 +0900

    기본 푸시 알림 받기 테스트

--- app/src/main/java/com/tje/pushfcmtest/fcm/MyMessageService.java
+++ app/src/main/java/com/tje/pushfcmtest/fcm/MyMessageService.java
@@ -1,9 +1,16 @@
 package com.tje.pushfcmtest.fcm;
 
+import android.util.Log;
+
 import com.google.firebase.messaging.FirebaseMessagingService;
 
 public class MyMessageService extends FirebaseMessagingService {
 
+    @Override
+    public void onNewToken(String s) {
+        super.onNewToken(s);
 
+        Log.d("새 토큰 발급", s);
 
+    }
 }
